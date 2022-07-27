9e2355eae6276fbca6a40c86aae8ae243d4e492c
nurselaine <Elaine.t.huynh@gmail.com>
2022-07-27 13:09:44 -0700

    created responsive navbar

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Navbar from './Navbar';
 import './Home.css';
 
 const Home = () => {
   return (
     <div className='home-container'>
       <div className='home-splash'>
-        <div className='hero'></div>
+        <div className='hero'>
+          <div><Navbar className='nav-bar' /></div>     
+          <div className='hero-title-container'>
+            <div className='hero-title'>It Doesn't Matter If Your Life is Perfect as Long as Your Hair Color is.</div>
+            <div className='btn-container'>
+            Book Appointment
+          </div>
+          </div>
+        </div> 
       </div>
     </div>
   )
