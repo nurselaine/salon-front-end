dd185988150ff192ea3b8abfac6b6681d1465c1a
nurselaine <Elaine.t.huynh@gmail.com>
2022-08-05 18:23:10 -0700

    created post request for appointment form

--- src/component/AppointmentForm.js
+++ src/component/AppointmentForm.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { GiVibratingSmartphone } from 'react-icons/gi';
+import { AiOutlineSmile } from 'react-icons/ai';
+import { ClipLoader } from 'react-spinners';
 import './AppointmentForm.css';
 
 const AppointmentForm = () => {
@@ -9,30 +11,64 @@ const AppointmentForm = () => {
   const [email, setEmail] = useState('');
   const [number, setNumber] = useState(1);
   const [service, setService] = useState('');
-  const [Message, setMessage] = useState('');
+  const [message, setMessage] = useState('');
+  const [messageSent, setMessageSent] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = (e) => {
     // send information to backend 
+    const data = {
+      name: name,
+      email: email,
+      number: number,
+      service: service,
+      message: message,
+    }
+    setLoading(true);
+    const url = `${process.env.REACT_APP_SERVER}/appointments`;
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error('error:', error);
+      });
+
+    // reset state
+    setName('');
+    setEmail('');
+    setNumber(1);
+    setService('');
+    setMessage('');
+    setMessageSent(true);
+    setLoading(false);
   }
 
   return (
     <div className='form-container'>
       <div className='contact-title-container'>
-        <GiVibratingSmartphone/>
+        <GiVibratingSmartphone />
         <h3 className='contact-title'>Get In Touch With Us</h3>
         <p className='contact-description'>Leave us a message and we will get back to you as soon as possible!</p>
       </div>
-      <Form>
+      <Form className={messageSent ? 'form-hidden' : 'form-show'}>
         <Form.Group className='form-input' required>
-          <Form.Control onChange={(e) => setName(e.target.value)} type='name' placeholder='Name*'/>
+          <Form.Control onChange={(e) => setName(e.target.value)} type='name' placeholder='Name*' />
         </Form.Group>
         <Form.Group onChange={(e) => setEmail(e.target.value)} className='form-input'>
-          <Form.Control type='email' placeholder='Email'/>
+          <Form.Control type='email' placeholder='Email' />
         </Form.Group>
         <Form.Group onChange={(e) => setNumber(e.target.value)} className='form-input' required>
-          <Form.Control type='phone' placeholder='Phone Number*'/>
+          <Form.Control type='phone' placeholder='Phone Number*' />
         </Form.Group>
-        <Form.Group onChange={(e) => setMessage(e.target.value)} className="form-input" required>
+        <Form.Group onChange={(e) => setService(e.target.value)} className="form-input" required>
           <Form.Select>
             <option value='' selected disabled>select a service</option>
             <option value='haircut'>Haircut</option>
@@ -41,13 +77,24 @@ const AppointmentForm = () => {
             <option value='treatment'>Treatment</option>
           </Form.Select>
         </Form.Group>
-        <Form.Group className='form-input'>
+        <Form.Group onChange={(e) => setMessage(e.target.value)} className='form-input'>
           <textarea class='form-control' rows='8' placeholder='Message*' required></textarea>
-          {/* <Form.Control rows='8' type='message' placeholder='Message*'/> */}
         </Form.Group>
       </Form>
-      <div className='form-btn-container'>
-        <Button id='form-btn' variant="dark" onClick={handleSubmit}>Send Message</Button>
+      {
+        loading ?
+          <ClipLoader/>
+          :
+          <div className={messageSent ? 'form-hidden' : 'form-show form-btn-container'}>
+            <Button id='form-btn' variant="dark" onClick={handleSubmit}>Send Message</Button>
+          </div>
+      }
+      <div className='submitted-container'>
+        <div className={messageSent ? 'submit' : 'not-Submit'}>
+          <AiOutlineSmile />
+          <h3 className='submit-title'>Your message has been sent!</h3>
+          <p className='submit-message'>Someone will contact you with appointment times and further information</p>
+        </div>
       </div>
     </div>
   )
